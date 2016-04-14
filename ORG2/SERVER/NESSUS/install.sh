41a5d4072b164a1357fdaccf8300f5785cfe6eae
José Luis Cánovas <joseluis@macbook.linuxmint>
2016-04-14 13:43:46 +0200

    Fix makefile scripts

--- ORG2/SERVER/NESSUS/install.sh
+++ ORG2/SERVER/NESSUS/install.sh
@@ -4,6 +4,6 @@ dpkg -i Nessus-6.6.0-ubuntu1110_amd64.deb
 
 /etc/init.d/nessusd start
 
-firefox server.org32:8834
+firefox https://server.org32:8834 &
 
 zenity --info --text '1. Aceptar certificado autogenerado.\n2. Aceptar Get Started\n3. Registar como admin:admin\n4. Código plugin:\n A6DD-7315-890C-078B-9144'
